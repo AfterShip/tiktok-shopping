3eb237a559c28799c63bb188480c620884002309
Henry-fw <fw.he@aftership.com>
2022-05-14 10:38:46 +0800

    initial release to 1.0.3

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'TikTok_Shopping',
+    'AfterShip_TikTokShopping',
     __DIR__
 );
